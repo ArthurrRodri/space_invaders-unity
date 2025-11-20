425aea08b64ad22945ea9a55f5c520ca901dd8e2
Boidones <BOILOLE200922@outlook.com>
2025-11-19 22:03:39 -0300

    placing the separate shooting script in PlayerMoviment.cs

---
+++ Assets/Scenes/Scripts/PlayerAttack.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float cooldown_;
+    [SerializeField] private float duration_;
+    bool isHit;
+
+    private void FixedUpdate() => BulletInstante();
+
+    private void BulletInstante()
+    {
+        if (Input.GetKey(KeyCode.Space) && isHit)
+        {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            isHit = false;
+            cooldown_ = 0f;
+        }
+        Cooldown();
+    }
+
+    private void Cooldown()
+    {
+        if (cooldown_ > duration_)
+        {
+            isHit = true;
+        }
+        else
+        {
+            cooldown_ += Time.deltaTime;
+        }
+    }
+}
--- Assets/Scenes/Scripts/PlayerMoviment.cs
+++ Assets/Scenes/Scripts/PlayerMoviment.cs
@@ -4,17 +4,13 @@ public class PlayerMoviment : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private GameObject bulletPrefab;
-    [SerializeField] private float cooldown_;
-    [SerializeField] private float duration_;
 
     bool isDead;
-    bool isHit;
+    
 
     void FixedUpdate()
     {
         Moviment();
-        BulletInstante();
     }
 
     private void Moviment()
@@ -25,29 +21,6 @@ public class PlayerMoviment : MonoBehaviour
         if (movement != Vector3.zero)
         {
             _rb.MovePosition(transform.position + movement * speed * Time.deltaTime);
-        }
-    }
-
-    private void BulletInstante()
-    { 
-        if (Input.GetKey(KeyCode.Space) && isHit)
-        {
-            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-            isHit = false;
-            cooldown_ = 0f;
-        }
-        Cooldown();
-    }
-
-    private void Cooldown()
-    {
-        if (cooldown_ > duration_)
-        {
-            isHit = true;
-        }
-        else
-        {
-            cooldown_ += Time.deltaTime;
         }
     }
 }
